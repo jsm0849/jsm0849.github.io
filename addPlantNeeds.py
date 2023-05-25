86405731665ba70f6879a06f28abab66ec6ae8c1
jsm0849 <jacob.smith0849@gmail.com>
2023-05-24 20:02:14 -0400

    added plant water needs and soil types to DB

--- addPlantNeeds.py
+++ addPlantNeeds.py
@@ -1,9 +1,27 @@
 import sqlite3
-from sqlite3 import Error
+import csv
 
-# Connecting to the database
+# This script adds the plantNeeds and soilTypes tables to the database. These tables hold the various included plant
+# types and the number of inches of water they need per week, and a variety of soil types and their ability to retain
+# water.
 connection = sqlite3.connect("DB.db")
 cursor = connection.cursor()
-cursor.execute("SELECT * FROM days")
-print(cursor.fetchone())
+cursor.execute("""CREATE TABLE plantNeeds
+                        (id TEXT, water REAL)""")
+cursor.execute("""CREATE TABLE soilTypes
+                        (id TEXT, retention REAL)""")
+with open("PlantWaterNeeds.csv", 'r') as plant_file:
+    plantReader = csv.reader(plant_file)
+    for row in plantReader:
+        command = "INSERT INTO plantNeeds VALUES('" + row[0] + "', '" + row[1] + "')"
+        cursor.execute(command)
+        connection.commit()
+plant_file.close()
+with open("SoilTypes.csv", 'r') as soil_file:
+    soilReader = csv.reader(soil_file)
+    for row in soilReader:
+        command = "INSERT INTO soilTypes VALUES('" + row[0] + "', '" + row[1] + "')"
+        cursor.execute(command)
+        connection.commit()
+soil_file.close()
 connection.close()
