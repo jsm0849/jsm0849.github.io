a0b18d69a6f3de598c5bdbdd304b78b22620d1d5
jsm0849 <jacob.smith0849@gmail.com>
2023-05-25 20:26:13 -0400

    more updates to GUI

--- sproutcast.py
+++ sproutcast.py
@@ -1,6 +1,15 @@
 import sqlite3
 import streamlit
 
+
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+
 page_title = "SproutCast: A Gardener's Companion"
 page_icon = ":seedling:"
 layout = "centered"
@@ -40,5 +49,22 @@ with streamlit.form("input_form"):
         for i in range(len(plant)):
             streamlit.checkbox(label=f"{plant[i]}", key=plant[i])
     col3.selectbox("Select Soil Type:", soil, key="selectedSoil")
-    col4.text_input("Enter the amount of extra water given in the last week in inches:", key="extraWater")
+    col4.text_input("Enter the amount of extra water given in the last week in inches:", key="extraWater", placeholder="0")
+    "---"
+    submitted = streamlit.form_submit_button("Submit Form")
+    if submitted:
+        inputZipcode = str(streamlit.session_state["inputZipcode"])
+        extraWater = str(streamlit.session_state["extraWater"])
+        if not inputZipcode.isDigit():
+            streamlit.error("Please enter a valid zip code.")
+        else:
+            inputZipcode = int(inputZipcode)
+            command = "SELECT * FROM zipcodes WHERE id = '" + inputZipcode + "'"
+            if cursor.execute(command) == None:
+                streamlit.error("Please enter a valid zip code.")
+            else:
+                if not is_number(extraWater):
+                    streamlit.error("Please enter a valid number of inches of water recently given.")
+                else:
+                    print("continue on")
 connection.close()
